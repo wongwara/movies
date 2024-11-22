6f98fa39c855f63ddaa7749a08356bc129d5ac68
wongwara.w <107904836+wongwara@users.noreply.github.com>
2024-11-23 03:40:32 +1100

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def fetch_movies(genre_id=None, randomize=True, limit=5):
     """Fetches movies using TMDb API based on genre and adult filter."""
     params = {
         "api_key": TMDB_API_KEY,
-        # "sort_by": "vote_average.desc",  # Sort by highest vote average
+        "sort_by": "vote_average.desc",  # Sort by highest vote average
         "include_adult": True,  # Only movies for adults
         "without_genres": "16",  # Exclude animated movies
         "primary_release_date.gte": "1999-01-01",  # Filter movies released after 1999
@@ -46,7 +46,7 @@ def fetch_tv_shows(genre_id=None, randomize=True, limit=5):
     """Fetches TV shows using TMDb API based on genre and adult filter."""
     params = {
         "api_key": TMDB_API_KEY,
-        # "sort_by": "vote_average.desc",  # Sort by highest vote average
+        "sort_by": "vote_average.desc",  # Sort by highest vote average
         "include_adult": True,  # Only TV shows for adults
         "without_genres": "16",  # Exclude animated TV shows
         "first_air_date.gte": "1999-01-01",  # Filter TV shows released after 1999
